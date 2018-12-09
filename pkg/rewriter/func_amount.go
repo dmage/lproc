704a7de37144d2c14aaf9404ebb41043a080a203
Oleg Bulatov <oleg@bulatov.me>
2018-12-09 18:41:32 +0100

    Add new amount format

--- pkg/rewriter/func_amount.go
+++ pkg/rewriter/func_amount.go
@@ -12,17 +12,31 @@ func init() {
 }
 
 var (
-	amount_re_1space000comma00 = regexp.MustCompile(`^(-?(?:[1-9][0-9 ]*)?[0-9]),([0-9][0-9])$`)
+	amount_re_1space000comma00 = regexp.MustCompile(`^([+-]?)((?:[1-9][0-9 ]*)?[0-9]),([0-9][0-9])$`)
+	amount_re_1000comma00      = regexp.MustCompile(`^([+-]?)((?:[1-9][0-9]*)?[0-9]),([0-9][0-9])$`)
 )
 
 func amount(pattern string, in string) string {
+	sign := func(x string) string {
+		if x == "+" {
+			return ""
+		}
+		return x
+	}
+
 	switch pattern {
 	case "1 000,00":
 		match := amount_re_1space000comma00.FindStringSubmatch(in)
 		if match == nil {
 			return ""
 		}
-		return fmt.Sprintf("%s.%s", strings.Replace(match[1], " ", "", -1), match[2])
+		return fmt.Sprintf("%s%s.%s", sign(match[1]), strings.Replace(match[2], " ", "", -1), match[3])
+	case "1000,00":
+		match := amount_re_1000comma00.FindStringSubmatch(in)
+		if match == nil {
+			return ""
+		}
+		return fmt.Sprintf("%s%s.%s", sign(match[1]), match[2], match[3])
 	}
 	return ""
 }
